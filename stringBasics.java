792ee79d907f500a5049f2ec23666b89880a87f7
crarun9740 <crarun9740@gmail.com>
2025-03-18 15:47:09 +0530

    string basic

--- stringBasics.java
+++ stringBasics.java
@@ -3,9 +3,11 @@ import java.util.Scanner;
 public class stringBasics {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        String s=sc.nextLine();
-        for(int i=2;i<4;i++){
-            System.out.print(s.substring(i));
+        String s="abcd";
+        for(int i=0;i<=3;i++){
+            for(int j=i+1;j<=4;j++){
+                System.out.print(s.substring(i,j)+" ");
+            }
         }
     }
 }
